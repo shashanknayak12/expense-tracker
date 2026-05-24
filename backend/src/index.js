66a9965bacfa9a1f9ad1c4a98a2ba953c8821311
Shashank Nayak <shashanknayak.74@gmail.com>
2026-05-24 22:35:50 +1000

    feat: add user modal and jwt auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import expensesRouter from "./routes/expenses.js";
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRouter);
 app.use("/api/expenses", expensesRouter);
 
 mongoose
