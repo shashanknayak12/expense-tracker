80bb7b464a340b0146209679296fdc1ea0ca42d9
Shashank Nayak <shashanknayak.74@gmail.com>
2026-05-24 22:59:13 +1000

    feat: add jwt middleware and protect expense route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import expensesRouter from "./routes/expenses.js";
 import authRouter from "./routes/auth.js";
+import { protect } from "./middleware/auth.js";
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/expenses", expensesRouter);
+app.use("/api/expenses", protect, expensesRouter);
 
 mongoose
   .connect(process.env.DATABASE_URL)
